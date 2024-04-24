f9994695b26fa716539267825963686886298b3a
Luke Marshall <lukemarshall2222@gmail.com>
2024-04-24 16:15:43 -0700

    create heap files

---
+++ priorityQueues/ts_priorityQ.ts
@@ -0,0 +1,11 @@
+/* Priority Queue through min-binary heap in TypeScript */
+
+
+class PriorityQueue {
+
+    values : Array<number>;
+
+    constructor(vals : Array<number>) {
+        this.values = vals;
+    }
+}
